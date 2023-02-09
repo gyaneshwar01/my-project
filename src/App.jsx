bd33d48d6af6a8884079a6bef0e194076356d7bf
Gyaneshwar Sah <gyaneshwar585@gmail.com>
2023-02-09 21:56:08 +0545

    Added delete and local storage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,30 @@ import "./App.css";
 export default function App() {
   const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    if (localStorage.getItem("products")) {
+      setProducts(JSON.parse(localStorage.getItem("products")));
+    }
+  }, []);
+
   const addProduct = (product) => {
-    setProducts((prevProducts) => [product, ...prevProducts]);
+    const newProducts = [product, ...products];
+    setProducts(newProducts);
+    localStorage.setItem("products", JSON.stringify(newProducts));
+  };
+
+  const deleteProduct = (id) => {
+    const newProducts = products.filter((product) => product.id !== id);
+    setProducts(newProducts);
+    localStorage.setItem("products", JSON.stringify(newProducts));
   };
 
   return (
     <div className="overflow-hidden">
       <AddProduct addProduct={addProduct} products={products} />
-      {products.length > 0 && <Products products={products} />}
+      {products.length > 0 && (
+        <Products products={products} deleteProduct={deleteProduct} />
+      )}
     </div>
   );
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { AiFillDelete } from "react-icons/ai";
 
-function Products({ products }) {
+function Products({ products, deleteProduct }) {
   return (
     <div className="container w-3/5 mx-auto">
       <table className="text-sm w-full text-left text-gray-500 dark:text-gray-400">
@@ -15,11 +16,14 @@ function Products({ products }) {
             <th scope="col" className="px-3 py-3 w-1/3">
               Price
             </th>
+            <th scope="col" className="px-3 py-3 w-1/3">
+              Action
+            </th>
           </tr>
         </thead>
         <tbody>
           {products.map((product) => (
-            <tr className="bg-white border-b text-md" key={product.id}>
+            <tr className="bg-white border-b text-lg" key={product.id}>
               <th
                 scope="row"
                 className="px-3 py-4 font-medium text-gray-900 whitespace-nowrap"
@@ -28,6 +32,15 @@ function Products({ products }) {
               </th>
               <td className="px-3 py-4">{product.category}</td>
               <td className="px-3 py-4">{product.price}</td>
+              <td className="px-3 py-4">
+                <button
+                  onClick={() => {
+                    deleteProduct(product.id);
+                  }}
+                >
+                  <AiFillDelete className="text-center text-xl" />
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
